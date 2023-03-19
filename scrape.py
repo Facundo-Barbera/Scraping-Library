485a3e66c80a2a978cca3365ddcff4ed2bd80270
facundo.barbera <facundo.barbera@gmail.com>
2023-03-19 16:55:47 -0600

    Updated scrape.py
    Updated the file to allow a second system argument "(output-file)". Can also be executed without arguments, in which case the program will ask for the scrapper and use the default database.

--- scrape.py
+++ scrape.py
@@ -8,22 +8,32 @@ from scrappers import QuotesToScrapeScrapper
 
 def main():
     if len(sys.argv) < 2:
-        print('Please provide a scrapper name as an argument.')
-        return
+        # Ask the user for the scrapper name
+        scrapper = input("Please enter the name of the scrapper you want to use: ")
 
-    if len(sys.argv) > 2 and sys.argv[2].endswith('.db'):
-        database_file = sys.argv[2]
-    else:
+        # Set the database file to None
         print('Using default database file (data/quotes.db)')
-        print('To use a different database file, provide it as the second argument.')
-        print('Example: python scrape.py quotes data/quotes.db')
-        print('Note: if you provided a database file and it did not work, make sure the file ends with .db')
         database_file = None
+    else:
+        # Get the scrapper name from the first argument
+        scrapper = sys.argv[1]
 
-    match sys.argv[1]:
+        # Check if a database file was provided
+        if len(sys.argv) > 2 and sys.argv[2].endswith('.db'):
+            database_file = sys.argv[2]
+        else:
+            print('Using default database file (data/quotes.db)')
+            print('To use a different database file, provide it as the second argument.')
+            print('Example: python scrape.py quotes data/quotes.db')
+            print('Note: if you provided a database file and it did not work, make sure the file ends with .db')
+            database_file = None
+
+    match scrapper:
         case 'quotes':
             scrapper = QuotesToScrapeScrapper(database_file if database_file else None)
             scrapper.scrape()
+        case _:
+            print(f'No scrapper named {scrapper} found.')
 
 
 if __name__ == '__main__':
