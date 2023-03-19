256ebd70cbb53c61ea204440c1b2ebc8efc3f53f
facundo.barbera <facundo.barbera@gmail.com>
2023-03-19 16:51:53 -0600

    Updated scrape.py
    Updated the file to allow a second system argument "(output-file)".

--- scrape.py
+++ scrape.py
@@ -8,13 +8,22 @@ from scrappers import QuotesToScrapeScrapper
 
 def main():
     if len(sys.argv) < 2:
-        print('Usage: python scrape.py <site_to_scrape>')
-        print('Sites to scrape: quotes')
-        sys.exit(1)
+        print('Please provide a scrapper name as an argument.')
+        return
 
-    if sys.argv[1] == 'quotes':
-        scrapper = QuotesToScrapeScrapper()
-        scrapper.scrape()
+    if len(sys.argv) > 2 and sys.argv[2].endswith('.db'):
+        database_file = sys.argv[2]
+    else:
+        print('Using default database file (data/quotes.db)')
+        print('To use a different database file, provide it as the second argument.')
+        print('Example: python scrape.py quotes data/quotes.db')
+        print('Note: if you provided a database file and it did not work, make sure the file ends with .db')
+        database_file = None
+
+    match sys.argv[1]:
+        case 'quotes':
+            scrapper = QuotesToScrapeScrapper(database_file if database_file else None)
+            scrapper.scrape()
 
 
 if __name__ == '__main__':
