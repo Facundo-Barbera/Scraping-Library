a9e7860775cc2ced19eeb0c468e71e55638b7eb7
facundo.barbera <facundo.barbera@gmail.com>
2023-03-19 20:05:16 -0600

    Added a testing unit to test QuotesToScrapeScrapper class

---
+++ tests/test_quotes_toscrape.py
@@ -0,0 +1,47 @@
+import unittest
+
+from scraping_library import QuotesToScrapeScrapper
+
+
+class TestQuotesToScrape(unittest.TestCase):
+
+    def test_quotes_max_0(self):
+        # Create instance of QuotesToScrapeScrapper
+        scrapper = QuotesToScrapeScrapper(max_quotes=0)
+
+        # Scrape quotes
+        scrapper.scrape()
+
+        # Get quotes
+        quotes = scrapper.get_quotes()
+
+        # Check if all quotes have the correct format and key
+        for quote in quotes:
+            self.assertEqual(len(quote), 3)
+            self.assertIn('text', quote)
+            self.assertIn('author', quote)
+            self.assertIn('tags', quote)
+
+    def test_quotes_max_20(self):
+        # Create instance of QuotesToScrapeScrapper
+        scrapper = QuotesToScrapeScrapper(max_quotes=20)
+
+        # Scrape quotes
+        scrapper.scrape()
+
+        # Get quotes
+        quotes = scrapper.get_quotes()
+
+        # Check if the number of quotes is correct
+        self.assertEqual(len(quotes), 20)
+
+        # Check if all quotes have the correct format and key
+        for quote in quotes:
+            self.assertEqual(len(quote), 3)
+            self.assertIn('text', quote)
+            self.assertIn('author', quote)
+            self.assertIn('tags', quote)
+
+
+if __name__ == '__main__':
+    unittest.main()
