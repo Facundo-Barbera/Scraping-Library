5178648cbed78488be78996b5cea5d8bb01e1000
facundo.barbera <facundo.barbera@gmail.com>
2023-03-19 21:12:04 -0600

    Updated test_quotes_toscrape.py
    Added a test for max_quotes 1000.
    Added a test for the database (Don't use it if you don't know what you are doing!).

--- tests/test_quotes_toscrape.py
+++ tests/test_quotes_toscrape.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from scraping_library import QuotesToScrapeScrapper
@@ -6,13 +7,9 @@ from scraping_library import QuotesToScrapeScrapper
 class TestQuotesToScrape(unittest.TestCase):
 
     def test_quotes_max_0(self):
-        # Create instance of QuotesToScrapeScrapper
-        scrapper = QuotesToScrapeScrapper(max_quotes=0)
-
-        # Scrape quotes
-        scrapper.scrape()
-
         # Get quotes
+        scrapper = QuotesToScrapeScrapper()
+        scrapper.scrape()
         quotes = scrapper.get_quotes()
 
         # Check if all quotes have the correct format and key
@@ -23,13 +20,9 @@ class TestQuotesToScrape(unittest.TestCase):
             self.assertIn('tags', quote)
 
     def test_quotes_max_20(self):
-        # Create instance of QuotesToScrapeScrapper
-        scrapper = QuotesToScrapeScrapper(max_quotes=20)
-
-        # Scrape quotes
-        scrapper.scrape()
-
         # Get quotes
+        scrapper = QuotesToScrapeScrapper(max_quotes=20)
+        scrapper.scrape()
         quotes = scrapper.get_quotes()
 
         # Check if the number of quotes is correct
@@ -42,15 +35,32 @@ class TestQuotesToScrape(unittest.TestCase):
             self.assertIn('author', quote)
             self.assertIn('tags', quote)
 
-    def test_quotes_database(self):
-        # Create instance of QuotesToScrapeScrapper
-        scrapper = QuotesToScrapeScrapper(max_quotes=20)
-
-        # Scrape quotes
+    def test_quotes_max_1000(self):
+        # Get quotes
+        scrapper = QuotesToScrapeScrapper(max_quotes=1000)
         scrapper.scrape()
+        quotes = scrapper.get_quotes()
 
-        # Save quotes to database
+        # Check if all quotes have the correct format and key
+        for quote in quotes:
+            self.assertEqual(len(quote), 3)
+            self.assertIn('text', quote)
+            self.assertIn('author', quote)
+            self.assertIn('tags', quote)
+
+    def test_quotes_database(self):
+        # Scrape quotes and save to database
+        scrapper = QuotesToScrapeScrapper(max_quotes=20, database_file='data/output.db')
+        scrapper.scrape()
         scrapper.save_to_database()
+
+        # Check if database file exists
+        self.assertTrue(os.path.isfile('data/output.db'))
+
+        # Clean up
+        os.remove('data/output.db')
+        if not os.listdir('data'):
+            os.rmdir('data')
 
 
 if __name__ == '__main__':
