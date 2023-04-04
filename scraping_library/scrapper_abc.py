8888f971f0fee8a5431f77f203bc1cb4d397e8c4
facundobautistabarbera <facundo.barbera@gmail.com>
2023-04-04 11:43:06 -0600

    Updated scrapper_abc.py
    - Deleted most of the abstract methods. Future classes should implement functions as necessary.
    - Changed _parse_html to _parse_html_from_url. It makes more sense now.
    - Removed the verbose argument on __init__ since now we are using logging (hurray).

--- scraping_library/scrapper_abc.py
+++ scraping_library/scrapper_abc.py
@@ -1,4 +1,3 @@
-import os
 from abc import ABC, abstractmethod
 
 import requests
@@ -13,47 +12,19 @@ class ScrapperABC(ABC):
     """
 
     @abstractmethod
-    def __init__(self, verbose: bool = False):
+    def __init__(self):
         """
         Initializes the ScrapperABC.
-
-        Args:
-            verbose: If True, the progress of the scraping will be printed to the console.
         """
-        self._verbose = verbose
 
     @abstractmethod
-    def scrape(self):
+    def _parse_html_from_url(self, url):
         """
-        Scrape a website.
-        This is a blueprint for all scraping_library scrapers.
+        Parses HTML from a URL.
         """
-        raise NotImplementedError
+        # Get HTML from URL and parse it
+        html = requests.get(url).text
+        parsed_html = BeautifulSoup(html, 'html.parser')
 
-    @abstractmethod
-    def _scrape_page(self, parsed_html):
-        """
-        Scrape a page.
-        This is a blueprint for all scraping_library scrapers.
-        """
-        raise NotImplementedError
-
-    def _parse_html(self, url: str):
-        """
-        Parse HTML from a URL using BeautifulSoup with html.parser.
-        """
-        if self._verbose:
-            print(f'Parsing HTML from {url}')
-        response = requests.get(url, timeout=5)
-        return BeautifulSoup(response.text, 'html.parser')
-
-    def _make_dirs_to_path(self, path: str):
-        """
-        Make directories to a path if they don't exist.
-        This function is used when the scrapper wants to save a file to a path that doesn't exist.
-        """
-        if self._verbose:
-            print(f'Making directories to {path}')
-
-        if not os.path.exists(os.path.dirname(path)):
-            os.makedirs(os.path.dirname(path))
+        # Return parsed HTML
+        return parsed_html
