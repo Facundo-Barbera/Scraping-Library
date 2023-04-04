0ad515e1fc35a0b04661cb3983342ded65c432c8
facundobautistabarbera <facundo.barbera@gmail.com>
2023-04-04 11:43:47 -0600

    Updated tests
    - Updated test_quotes_toscrape.py to match the new format of the scrapper.
    - Added a new test for scrape_preloade function.

--- tests/test_quotes_toscrape.py
+++ tests/test_quotes_toscrape.py
@@ -1,14 +1,22 @@
 import unittest
 
-from scraping_library import QuotesToScrapeScrapper
+from scraping_library import QuotesToScrape
 
 
 class TestQuotesToScrape(unittest.TestCase):
 
     def test_scraping(self):
         # Scrape all quotes.
-        scrapper = QuotesToScrapeScrapper(verbose=True)
-        quotes = scrapper.scrape(max_quotes=0)
+        scrapper = QuotesToScrape()
+        quotes = scrapper.scrape()
+
+        # Check that there are more than 0 quotes.
+        self.assertGreater(len(quotes), 0)
+
+    def test_scraping_preloaded(self):
+        # Scrape all quotes.
+        scrapper = QuotesToScrape()
+        quotes = scrapper.scrape_preloaded()
 
         # Check that there are more than 0 quotes.
         self.assertGreater(len(quotes), 0)
