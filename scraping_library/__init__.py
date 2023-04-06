1e7361d6ad6b0557b03c33835c15ca9e332dd40f
facundobautistabarbera <facundo.barbera@gmail.com>
2023-04-06 09:50:33 -0600

    Updated books to scrape
    - Created the BooksToScrape class.
    - Added full functionality for the scrape function.
      - This function works asynchronously.

--- scraping_library/__init__.py
+++ scraping_library/__init__.py
@@ -1,2 +1,3 @@
+from .books_toscrape import BooksToScrape
 from .quotes_toscrape import QuotesToScrape
 from .scrapper_abc import DEFAULT_DATABASE_FILE
--- scraping_library/books_toscrape.py
+++ scraping_library/books_toscrape.py
@@ -1,71 +1,183 @@
+import asyncio
+import logging
+import time
+
+import aiohttp
+import requests
 from bs4 import BeautifulSoup
 
-from .scrapper import Scrapper, DEFAULT_DATABASE_FILE
+from scraping_library.scrapper_abc import ScrapperABC
 
 
-class BooksToScrapeScrapper(Scrapper):
+class BooksToScrape(ScrapperABC):
     """
-    Scrapes books from http://books.toscrape.com
-
-    Saves the books in a sqlite3 (.db) file.
+    Scrapes books from https://books.toscrape.com
     """
-
     BASE_URL = 'http://books.toscrape.com'
+    RATING_TO_STARS = {
+        'One': 1,
+        'Two': 2,
+        'Three': 3,
+        'Four': 4,
+        'Five': 5,
+    }
 
     def __init__(self):
         """
         Initializes the BooksToScrapeScrapper.
         """
-        self._books = []
-        self._current_url = self.BASE_URL
+        # Setup logger
+        logging.basicConfig(level=logging.INFO, format='[BooksToScrape] %(levelname)s - %(message)s')
+        self._logger = logging.getLogger(__name__)
 
-    def scrape(self, max_books: int = 0):
+    def scrape(self, semaphore_=15):
         """
-        Scrapes books from http://books.toscrape.com.
-
-        Args:
-            max_books: The maximum number of books to scrape. If 0, all books will be scraped.
-                If the number of books is less than max_books, all books will be scraped.
-        """
-        # Scrape books
-        pass
-
-    def save_to_database(self, database_file: str = DEFAULT_DATABASE_FILE):
-        """
-        Saves the scraped books to the database.
+        Scrapes books from http://books.toscrape.com asynchronously.
 
         Args:
-            database_file: The database file to save the books to. Must be a .db file.
-        """
-        pass
-
-    def get_books(self):
-        """
-        Returns:
-            A list of books.
-        """
-        return self._books
-
-    def _scrape_books(self, parsed_html: BeautifulSoup):
-        """
-        Scrapes books from a parsed HTML.
-
-        Args:
-            parsed_html: The parsed HTML to scrape books from.
+            async_: Whether to scrape asynchronously or not.
+            semaphore_: The semaphore to use when scraping asynchronously.
 
         Returns:
             A list of books.
         """
-        books = []
+        start_time = time.time()
+        self._logger.info('Scraping...')
 
+        # Get all catalogue pages
+        parsed_catalogue_htmls = self._get_all_catalogue_pages()
 
+        # Get all book pages from all catalogue pages
+        async def get_all_book_htmls(parsed_htmls):
+            book_html_start_time = time.time()
+            self._logger.info('Getting all book pages...')
+            sem = asyncio.Semaphore(semaphore_)
 
-book = {
-    'title': 'A Light in the Attic',
-    'author': 'Suzanne Collins',
-    'price': '£51.77',
-    'rating': 'Three',
-    'image_url': 'http://books.toscrape.com/media/cache/2c/da/2cdad67c44b002e7ead0cc35693c0e8b.jpg',
-    'book_url': 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html',
+            async def _get_book_html_with_semaphore(parsed_html):
+                async with sem:
+                    return await self._get_books_htmls(parsed_html)
 
-}
+            tasks = [_get_book_html_with_semaphore(parsed_html) for parsed_html in parsed_htmls]
+            book_pages = [book for books in await asyncio.gather(*tasks) for book in books]
+            self._logger.info(f'Got {len(book_pages)} book pages in {time.time() - book_html_start_time} seconds.')
+            return book_pages
+
+        parsed_book_htmls = asyncio.run(get_all_book_htmls(parsed_catalogue_htmls))
+
+        # Get all book data from all book pages
+        async def get_all_book_data(parsed_htmls):
+            book_data_start_time = time.time()
+            self._logger.info('Getting all book data...')
+            sem = asyncio.Semaphore(semaphore_)
+
+            async def _get_book_data_with_semaphore(parsed_html):
+                async with sem:
+                    return await self._extract_book_data(parsed_html)
+
+            tasks = [_get_book_data_with_semaphore(parsed_html) for parsed_html in parsed_htmls]
+            book_data = await asyncio.gather(*tasks)
+            self._logger.info(f'Got {len(book_data)} book data in {time.time() - book_data_start_time} seconds.')
+            return book_data
+
+        parsed_book_data = asyncio.run(get_all_book_data(parsed_book_htmls))
+
+        # Return book data
+        self._logger.info(f'Scraped {len(parsed_book_htmls)} books in {time.time() - start_time} seconds.')
+        return parsed_book_data
+
+    def _get_all_catalogue_pages(self):
+        """
+        Gets all catalogue pages.
+
+        Returns:
+            A list of parsed HTMLs.
+        """
+        # Get HTML from base URL and parse it
+        parsed_htmls = [self._parse_html_from_url(self.BASE_URL)]
+
+        while True:
+            next_button = parsed_htmls[-1].find('li', {'class': 'next'})
+            next_page = next_button.find('a')['href'] if next_button else None
+
+            if not next_page:
+                break
+            else:
+                parsed_htmls.append(self._parse_html_from_url(
+                    self.BASE_URL + f'{"/" if next_page.startswith("catalogue") else "/catalogue/"}' + next_page)
+                )
+
+        return parsed_htmls
+
+    async def _get_books_htmls(self, parsed_html):
+        """
+        Gets all books htmls from a parsed html asynchronously.
+
+        Args:
+            parsed_html: The parsed HTML of the page.
+
+        Returns:
+            A list of books URLs.
+        """
+        books_urls = []
+
+        # Get all books
+        books = parsed_html.find_all('article', {'class': 'product_pod'})
+
+        # Get all books URLs
+        for book in books:
+            url = book.find('a')['href']
+            books_urls.append(self.BASE_URL + f'{"/" if url.startswith("catalogue") else "/catalogue/"}' + url)
+
+        async with aiohttp.ClientSession() as _:
+            tasks = [self._get_page_async(book_url) for book_url in books_urls]
+            return await asyncio.gather(*tasks)
+
+    async def _extract_book_data(self, parsed_html):
+        """
+        Extracts book data from a parsed HTML.
+
+        Args:
+            parsed_html: The parsed HTML of the book page.
+
+        Returns:
+            A dictionary of book data.
+        """
+
+        def get_element_safe(soup, func, *args, **kwargs):
+            try:
+                return func(soup, *args, **kwargs)
+            except AttributeError:
+                return None
+            except ValueError:
+                return None
+
+        book_data = {
+            'title': get_element_safe(parsed_html, lambda s: s.find('h1').text),
+            'genre': get_element_safe(parsed_html,
+                                      lambda s: s.find('ul', {'class': 'breadcrumb'}).find_all('li')[2].text),
+            'price': get_element_safe(parsed_html, lambda s: float(s.find('p', {'class': 'price_color'}).text[2:])),
+            'rating': get_element_safe(parsed_html, lambda s: self.RATING_TO_STARS[
+                s.find('p', {'class': 'star-rating'})['class'][1]]),
+            'image_url': get_element_safe(parsed_html, lambda s: self.BASE_URL + '/' + s.find('img')['src'][6:]),
+            'stock': get_element_safe(parsed_html, lambda s: int(
+                s.find('p', {'class': 'instock availability'}).text.split()[2][1:-1])),
+            'description': get_element_safe(parsed_html,
+                                            lambda s: s.find('div', {'id': 'product_description'}).find_next_sibling(
+                                                'p').text),
+            'reviews': get_element_safe(parsed_html, lambda s: int(
+                s.find('table', {'class': 'table table-striped'}).find_all('tr')[6].find_all('td')[0].text))
+        }
+
+        return book_data
+
+    def _parse_html_from_url(self, url):
+        """
+        Parses HTML from a URL.
+        """
+        # Get HTML from URL and parse it
+        self._logger.info(f'Parsing HTML from {url}...')
+        html = requests.get(url).text
+        parsed_html = BeautifulSoup(html, 'html.parser')
+
+        # Return parsed HTML
+        return parsed_html
