3c04eec764827bc93994bf9f1aeb9793d4038848
facundobautistabarbera <facundo.barbera@gmail.com>
2023-04-06 10:16:28 -0600

    Project cleanup
    - Removed comments that were used during development.

--- tests/test_books_toscrape.py
+++ tests/test_books_toscrape.py
@@ -6,19 +6,13 @@ from scraping_library import BooksToScrape
 class TestBooksToScrape(unittest.TestCase):
 
     def test_scraping_no_async(self):
-        # Scrape all quotes.
         scrapper = BooksToScrape()
         books = scrapper.scrape(semaphore_=1)
-
-        # Check that there are more than 0 quotes.
         self.assertGreater(len(books), 0)
 
     def test_scraping_async(self):
-        # Scrape all quotes.
         scrapper = BooksToScrape()
         books = scrapper.scrape(semaphore_=15)
-
-        # Check that there are more than 0 quotes.
         self.assertGreater(len(books), 0)
 
 
